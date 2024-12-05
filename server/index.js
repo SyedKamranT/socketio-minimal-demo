eee425e92c3995c1db5f156f071d1d4d1c625097
Syed Kamran <Unitygame1380@gmail.com>
2024-12-05 16:17:16 +0530

    s

--- server/index.js
+++ server/index.js
@@ -1,36 +1,18 @@
+const { Socket } = require('socket.io');
 
 const http = require('http').createServer();
 
 const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+    cors: {origin:"*"}
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-
-    socket.on('message', (message) =>     {
+io.on('connection', (socket)  => {
+    console.log('a new client connected');
+    socket.on('message', (message) => {
         console.log(message);
-        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
+        io.emit('message', `${socket.id.substr(0,2)} said ${message}`);
     });
+
 });
 
-http.listen(8080, () => console.log('listening on http://localhost:8080') );
-
-
-// Regular Websockets
-
-// const WebSocket = require('ws')
-// const server = new WebSocket.Server({ port: '8080' })
-
-// server.on('connection', socket => { 
-
-//   socket.on('message', message => {
-
-//     socket.send(`Roger that! ${message}`);
-
-//   });
-
-// });
-
-
- 
+http.listen(8080), () => console.log('listening http://localhost:8080');
